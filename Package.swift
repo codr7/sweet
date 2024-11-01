829f146554bb4fd2b33f6a18687e3b22a8b6e8be
Andreas Nilsson <codr7@protonmail.com>
2024-11-01 04:05:39 +0100

    Add NIO.

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
 
   dependencies: [
     .package(url: "https://github.com/apple/swift-system", from: "1.4.0"),
+    .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
   ],
   
   targets: [
@@ -20,6 +21,9 @@ let package = Package(
       name: "sweet",
       dependencies: [
         .product(name: "SystemPackage", package: "swift-system"),
+        .product(name: "NIOCore", package: "swift-nio"),
+        .product(name: "NIOEmbedded", package: "swift-nio"),
+        .product(name: "NIOPosix", package: "swift-nio"),
       ]),
   ]
 )
